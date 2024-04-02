84f66268d07429cf82cc4d77d3f0e6cc7c770b68
Hoài Dương <127805551+duongvuive@users.noreply.github.com>
2024-04-02 23:00:52 +0700

    Chinh cau hinh start

--- src/app.js
+++ src/app.js
@@ -1,18 +1,24 @@
-require('dotenv').config({path: '../.env'}); // Sử dụng biến môi trường
+require('dotenv').config(); // Sử dụng biến môi trường từ tệp .env
+
 const express = require('express');
-const port = process.env.PORT;
 const app = express();
+const port = process.env.PORT || 3000; // Sử dụng mặc định là 3000 nếu PORT không được định nghĩa trong .env
+
 const authRoutes = require('./Routes/api/userRoutes');
-const adminRoutes=require('./Routes/api/adminUserRoutes');
+const adminRoutes = require('./Routes/api/adminUserRoutes');
 const userWeb = require('./Routes/web/userWebRoute');
-const configViewEngie=require('./config/viewEngie');
-configViewEngie(app);
+const configViewEngine = require('./config/viewEngie');
+
+configViewEngine(app);
+
 app.use(express.urlencoded({ extended: true }));
-app.use('/',userWeb);
-//Dùng cho API
+app.use('/', userWeb);
+
+// Sử dụng JSON middleware cho API
 app.use(express.json());
+
 app.use('/api', authRoutes);
-app.use('/api',adminRoutes);
+app.use('/api', adminRoutes);
 
 // Khởi động máy chủ và lắng nghe cổng được chỉ định
 app.listen(port, () => {
